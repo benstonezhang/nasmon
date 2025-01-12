f729951e622de3653f65f4dc18a4fbec13dab75e
Benstone Zhang <benstonezhang@gmail.com>
2025-01-12 18:02:36 +0800

    support HTTP get request
    Signed-off-by: benstone <benstonezhang@gmail.com>

--- sts_srv.c
+++ sts_srv.c
@@ -4,15 +4,25 @@
 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
 #include <syslog.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <assert.h>
 
 #include "nasmon.h"
 
 static int fd = -1;
+static int http_hdr_len;
+static char *http_hdr = "HTTP/1.1 200\r\n"
+                        "Connection: Keep-Alive\r\n"
+                        "Keep-Alive: timeout=60, max=180\r\n"
+                        "Content-Type: application/json\r\n"
+                        "Content-Length: ";
+static char send_buf[2048];
 
 void nas_stssrv_free(void) {
     if (fd >= 0) {
@@ -22,6 +32,8 @@ void nas_stssrv_free(void) {
 }
 
 int nas_stssrv_init(const short port) {
+    http_hdr_len = strlen(http_hdr);
+
     if ((fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) == -1) {
         syslog(LOG_WARNING, "failed create TCP server socket");
         nas_log_error();
@@ -31,7 +43,10 @@ int nas_stssrv_init(const short port) {
     atexit(nas_stssrv_free);
 
     int optval = 1;
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
+        nas_log_error();
+        exit(EXIT_FAILURE);
+    }
 
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
@@ -40,13 +55,13 @@ int nas_stssrv_init(const short port) {
 
     syslog(LOG_INFO, "start status server at port %d\n", port);
 
-    if (bind(fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+    if (bind(fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
         syslog(LOG_ERR, "failed bind address and port");
         nas_log_error();
         exit(EXIT_FAILURE);
     }
 
-    if (listen(fd, 2) != 0) {
+    if (listen(fd, 8) != 0) {
         syslog(LOG_ERR, "failed listen the socket");
         nas_log_error();
         exit(EXIT_FAILURE);
@@ -81,20 +96,61 @@ int nas_stssrv_to_json(char *buf, const size_t len) {
     return count;
 }
 
+int nas_sts_skt_write(int fd, char *buf, int count) {
+    int rc, off = 0;
+    while (off < count) {
+        rc = write(fd, buf + off, count - off);
+        if (rc <= 0) {
+            if ((errno == EINTR) || (errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+                continue;
+            }
+            return rc;
+        }
+        off += rc;
+    }
+    return count;
+}
+
 void nas_stssrv_export(void) {
     struct sockaddr_in accept_addr;
     unsigned int accept_addr_len = sizeof(accept_addr);
+    int http = 0;
+    int offset = 0;
+    char buf[1920];
 
-    int client_fd = accept(fd, (struct sockaddr *) &accept_addr, &accept_addr_len);
+    int client_fd = accept(fd, (struct sockaddr *)&accept_addr, &accept_addr_len);
     if (client_fd < 0) {
         syslog(LOG_WARNING, "failed accept client connection");
         nas_log_error();
         return;
     }
 
-    char buf[2048];
+    /* Set socket to be nonblocking. */
+    int optval = fcntl(client_fd, F_GETFL, 0);
+    if (fcntl(client_fd, F_SETFL, optval | O_NONBLOCK) < 0) {
+        nas_log_error();
+        return;
+    }
+
+    if ((read(client_fd, buf, 4) == 4) &&
+        (strncmp(buf, "GET ", 4) == 0)) {
+        http = 1;
+    }
+
     int count = nas_stssrv_to_json(buf, sizeof(buf));
-    if (write(client_fd, buf, count) != count) {
+    if (http) {
+        strncpy(send_buf, http_hdr, http_hdr_len);
+        offset = http_hdr_len;
+        offset += snprintf(send_buf + offset, sizeof(send_buf) - offset, "%d\r\n\r\n", count + 2);
+    }
+    strncpy(send_buf + offset, buf, sizeof(send_buf) - offset);
+    offset += count;
+    if (http) {
+        strncpy(send_buf + offset, "\r\n", sizeof(send_buf) - offset);
+        offset += 2;
+    }
+
+    if (nas_sts_skt_write(client_fd, send_buf, offset) != offset) {
         syslog(LOG_WARNING, "failed write status to socket");
     }
 
